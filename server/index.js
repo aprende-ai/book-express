8652e1f48c5e4b3a896bfe17183fd33581e3b47f
Danilo Rechi <2472247+dansoah@users.noreply.github.com>
2020-08-18 00:04:29 -0300

    Adicionando object creator e suas dependencias

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
 const path = require('path');
 const db = require('./lib/db');
+const Autor = require('./lib/autor');
+const Editora = require('./lib/editora');
+const Livro = require('./lib/livro');
+
 
 const jsonFilePath = path.join(__dirname,'db.json');
 db.setJsonFilePath(jsonFilePath);
+db.setObjectCreator( function(collection, item){
+    switch(collection){
+        case "livros":
+            return new Livro()
+                .setTitulo(item.titulo)
+                .setIsbn(item.isbn)
+                .setPreco(item.preco)
+                .setUnidadesEstoque(item.unidadesEstoque);
 
-const Livro = require('./lib/livro');
-const Autor = require('./lib/autor');
-const Editora = require('./lib/editora');
+        case "editoras":
+            return new Editora()
+                .setNome(item.nome);
 
-const l = new Livro();
-l.save();
-
-const a = new Autor();
-a.save();
-
-const e = new Editora();
-e.save();
+        case "autores":
+            return new Autor()
+                .setNome(item.nome)
+                .setDataNascimento(item.dataNascimento);
+    }
+})
+db.getInstance();
