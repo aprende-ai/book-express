86f72925f0ef025187d64c9018181877a60d40bd
Danilo Rechi <2472247+dansoah@users.noreply.github.com>
2020-08-17 22:30:47 -0300

    Adicionando lógica para salvar objetos no arquivo json

--- server/lib/db/db.js
+++ server/lib/db/db.js
@@ -11,7 +11,32 @@ DB.prototype.sync = function(){
 }
 
 DB.prototype.save = function(){
-    const strData = JSON.stringify(this.data);
+
+    const paraSalvar = {};
+
+    const collections = Object.keys(this.data);
+    let collectionIdx = collections.length -1;
+
+    do{
+
+        const collectionName = collections[collectionIdx];
+
+        if(typeof paraSalvar[collectionName] === 'undefined'){
+            paraSalvar[collectionName] = [];
+        }
+
+        let itensIdx = 0;
+        while(itensIdx <= this.data[collectionName].length-1){
+            const item = this.data[collectionName][itensIdx];
+            paraSalvar[collectionName].push(item.toObject());
+            itensIdx++;
+        }
+
+        collectionIdx--;
+
+    }while(collectionIdx >= 0)
+
+    const strData = JSON.stringify(paraSalvar);
     fs.writeFileSync(this.dbFileLocation, strData);
 }
 
