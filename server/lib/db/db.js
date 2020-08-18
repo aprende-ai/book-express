926ef69055f9eb71f0cd5a9e2fab95878fb9021c
Danilo Rechi <2472247+dansoah@users.noreply.github.com>
2020-08-18 00:02:51 -0300

    Adicionando suporte ao objectCreator e adicionando lógica de sincronização com objetos

--- server/lib/db/db.js
+++ server/lib/db/db.js
@@ -1,13 +1,37 @@
 const fs = require('fs');
 
-function DB(dbFileLocation){
+function DB(dbFileLocation, objectCreator){
     this.dbFileLocation = dbFileLocation;
-    this.data = null;
+    this.objectCreator = objectCreator;
+    this.data = {};
 }
 
 DB.prototype.sync = function(){
-    const data = fs.readFileSync(this.dbFileLocation);
-    this.data = JSON.parse(data);
+
+    const conteudoArquivo = fs.readFileSync(this.dbFileLocation);
+    const data = JSON.parse(conteudoArquivo);
+    const collections = Object.keys(data);
+
+    let totalCollections = collections.length-1;
+
+    while(totalCollections>=0){
+        const collectionName = collections[totalCollections];
+        const collection = data[collectionName]
+        let totalItens = collection.length;
+
+        for(let i=0; i<=totalItens-1; i++){
+            const item = collection[i];
+            const obj = this.objectCreator(collectionName, item);
+            if( typeof this.data[collectionName] === 'undefined' ){
+                this.data[collectionName] = [];
+            }
+
+            this.data[collectionName].push(obj);
+        }
+        totalCollections--;
+    }
+
+    console.log(this.data);
 }
 
 DB.prototype.save = function(){
