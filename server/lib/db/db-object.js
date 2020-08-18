b1a51af7882a508f8cc97a439748b82f31b71512
Danilo Rechi <2472247+dansoah@users.noreply.github.com>
2020-08-17 22:31:28 -0300

    Adicionando lógica para permitir que objetos chamem o método de salvamento do db

--- server/lib/db/db-object.js
+++ server/lib/db/db-object.js
@@ -1,12 +1,16 @@
 const db = require('./index');
 
 function DBObject(collection){
-    this.collection = collection;
+    this.collectionName = collection;
     this.instance = db.getInstance();
+    this.collection = this.instance.data[this.collectionName];
 }
 
 DBObject.prototype.save = function(){
-    console.log('save!', this.collection);
+
+    this.collection.push(this);
+    this.instance.save();
+    
 }
 
 module.exports = DBObject;
