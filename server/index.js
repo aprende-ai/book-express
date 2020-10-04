f3c376223b1c0ea39a80402221e941731003cd22
Danilo Rechi <2472247+dansoah@users.noreply.github.com>
2020-10-04 19:29:16 -0300

    Exportando funções para inicialização do servidor

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const router = require('./router');
 const path = require('path');
 const db = require('./lib/db');
 const Autor = require('./lib/autor');
@@ -5,27 +7,52 @@ const Editora = require('./lib/editora');
 const Livro = require('./lib/livro');
 
 
-const jsonFilePath = path.join(__dirname,'db.json');
-db.setJsonFilePath(jsonFilePath);
-db.setObjectCreator( function(collection, item){
-    switch(collection){
-        case "livros":
-            return new Livro()
-                .setTitulo(item.titulo)
-                .setIsbn(item.isbn)
-                .setPreco(item.preco)
-                .setUnidadesEstoque(item.unidadesEstoque);
+const initializeDatabase = () => {
 
-        case "editoras":
-            return new Editora()
-                .setNome(item.nome);
+    const jsonFilePath = path.join(__dirname,'db.json');
+    db.setJsonFilePath(jsonFilePath);
+    db.setObjectCreator( function(collection, item){
+        switch(collection){
+            case "livros":
+                return new Livro()
+                    .setTitulo(item.titulo)
+                    .setIsbn(item.isbn)
+                    .setPreco(item.preco)
+                    .setUnidadesEstoque(item.unidadesEstoque);
 
-        case "autores":
-            return new Autor()
-                .setNome(item.nome)
-                .setDataNascimento(item.dataNascimento);
-    }
-})
-const instance = db.getInstance();
+            case "editoras":
+                return new Editora()
+                    .setNome(item.nome);
 
-console.log(instance.data.livros[0]);
+            case "autores":
+                return new Autor()
+                    .setNome(item.nome)
+                    .setDataNascimento(item.dataNascimento);
+        }
+    })
+
+}
+
+const initializeWebServer = (port, callback) => {
+    const server = express();
+    server.use(router);
+    server.listen(port, callback);
+
+}
+
+
+module.exports = {
+    initializeDatabase,
+    initializeWebServer
+}
+
+//
+// const port = 3000
+//
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+//
+// app.listen(port, () => {
+//   console.lhttps://runkit.com/og(`Example app listening at http://localhost:${port}`)
+// })
